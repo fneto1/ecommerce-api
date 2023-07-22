5f585b097e9361cfad3a03868bd2c7eb9f6192b0
fneto1 <fasneto2@gmail.com>
2023-07-22 11:30:10 -0300

    API de um ecommerce

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
+const cors = require("cors");
 
 const dbUser = process.env.DB_USER;
 const dbPass = process.env.DB_PASSWORD;
@@ -20,15 +21,24 @@ mongoose
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+//solve CORS
+app.use(cors({ credentials: true, origin: "http://localhost:3000" }));
+
 //endpoints
 
 const userRoutes = require("./routes/user");
 const authRoutes = require("./routes/auth");
 const productRoutes = require("./routes/product");
+const cartRoutes = require("./routes/cart");
+const orderRoutes = require("./routes/order");
+const stripeRoutes = require("./routes/stripe");
 
 app.use("/api/users", userRoutes);
 app.use("/api/auth", authRoutes);
 app.use("/api/products", productRoutes);
+app.use("/api/carts", cartRoutes);
+app.use("/api/orders", orderRoutes);
+app.use("/api/checkout", stripeRoutes);
 
 app.listen(port, () => {
   console.log("Backend server is running!");
